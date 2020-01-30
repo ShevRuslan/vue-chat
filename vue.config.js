6e1d460fb53c958d27d0492cf85a8963abea855f
sasa02sasa <sasa02sasa@mail.ru>
2020-01-30 22:51:19 +0600

    Добавлен quasar

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,14 @@
 module.exports = {
   devServer: {
     proxy: 'http://localhost:3001'
-  }
-}
+  },
+
+  pluginOptions: {
+    quasar: {
+      importStrategy: 'kebab',
+      rtlSupport: true
+    }
+  },
+
+  transpileDependencies: ['quasar']
+};
