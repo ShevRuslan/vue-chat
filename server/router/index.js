a1a310d06e787a7324a7dfc6eae2fb0fa49e889f
LilKoi <46331319+LilKoi@users.noreply.github.com>
2020-02-01 13:42:42 +0400

    create signIn

--- server/router/index.js
+++ server/router/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const router = express.Router();
-
+const router  = express.Router();
+const auth    = require('../controllers/auth');
 router.get('/test', (request, response) => {
   response.status(500).json({ message: 'Иди работай!' });
 });
+
+router.post('/login', auth.signIn);
 
 module.exports = router;
