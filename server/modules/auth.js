589e4155207db7c65aec8594db5d907ac23d6daa
LilKoi <46331319+LilKoi@users.noreply.github.com>
2020-02-01 19:13:32 +0400

    Авторизация переписана на классы

--- server/modules/auth.js
+++ server/modules/auth.js
@@ -4,52 +4,48 @@ const bcryptjs = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { jwtSecret } = require('../config/config.json');
 
-const singin = async (req, res) => {
-  //Регистрация пользователя
-  try {
-    const { login, password } = req.body; //Получение значений из формы
-    const search = User.findOne({ login: login }).exec(); //Поиск юзера с таким же логином
-    if (!search) {
-      //Если не был найден, сообщить об этом
-      res.json(`Пользователь с логином ${login} не найден`);
+module.exports = class {
+  singin = async (req, res) => {
+    //Регистрация пользователя
+    try {
+      const { login, password } = req.body; //Получение значений из формы
+      const search = User.findOne({ login: login }).exec(); //Поиск юзера с таким же логином
+      if (!search) {
+        //Если не был найден, сообщить об этом
+        res.json(`Пользователь с логином ${login} не найден`);
+      }
+      const valid = bcryptjs.compareSync(password, User.password);
+      if (valid) {
+        const token = jwt.sign(User._id.toString(), jwtSecret);
+        res.json({ token });
+      } else {
+        res.status(400).json('ошибка в валидации');
+      }
+    } catch (err) {
+      return err;
     }
-    const valid = bcryptjs.compareSync(password, User.password);
-    if (valid) {
-      const token = jwt.sign(User._id.toString(), jwtSecret);
-      res.json({ token });
-    } else {
-      res.status(400).json('ошибка в валидации');
+  };
+  signup = async (req, res) => {
+    try {
+      const { login, password, repeatPassword, secretKey } = req.body;
+      const search = User.findOne({ login: login }).exec(); //Поиск юзера с таким же логином
+      if (search) {
+        //Если найден, сообщить об этом
+        res.json(`Пользователь с логином ${login} уже существует. Придумайте новый`);
+      }
+      if (password !== repeatPassword) {
+        res.status(400).json('Пароли не совпадают');
+      }
+      const passwordHash = jwt(password, jwtSecret);
+      const complete = User.create({
+        login,
+        password: passwordHash,
+        secretKey
+      });
+      res.status(200).json('Готово');
+      res.json({ complete });
+    } catch (err) {
+      return err;
     }
-  } catch (err) {
-    return err;
-  }
-};
-
-const signup = async (req, res) => {
-  try {
-    const { login, password, repeatPassword, secretKey } = req.body;
-    const search = User.findOne({ login: login }).exec(); //Поиск юзера с таким же логином
-    if (search) {
-      //Если найден, сообщить об этом
-      res.json(`Пользователь с логином ${login} уже существует. Придумайте новый`);
-    }
-    if (password !== repeatPassword) {
-      res.status(400).json('Пароли не совпадают');
-    }
-    const passwordHash = jwt(password, jwtSecret);
-    const complete = User.create({
-      login,
-      password: passwordHash,
-      secretKey
-    });
-    res.status(200).json('Готово');
-    res.json({ complete });
-  } catch (err) {
-    return err;
-  }
-};
-
-module.exports = {
-  singin,
-  signup
+  };
 };
