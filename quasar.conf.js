98da29b79f0a2bc4f0ad5d93554b57997134e42d
sasa02sasa <sasa02sasa@mail.ru>
2020-02-02 00:12:41 +0600

    Добавлен плагин для оповещения в Quasar

--- quasar.conf.js
+++ quasar.conf.js
@@ -1,5 +1,11 @@
 export default {
   framework: {
     components: ['QCard', 'QCardSection', 'QCardActions, QInput, QForm, QBtn']
+  },
+  plugins: ['Notify'],
+  config: {
+    notify: {
+      /* Notify defaults */
+    }
   }
 };
