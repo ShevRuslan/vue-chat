f464f05d612c35945587758cbfba8ffd3c14190d
sasa02sasa <sasa02sasa@mail.ru>
2020-02-06 17:21:23 +0600

    Добавлены дополнительные компоненты из Quasar`а

--- quasar.conf.js
+++ quasar.conf.js
@@ -1,6 +1,21 @@
 export default {
   framework: {
-    components: ['QCard', 'QCardSection', 'QCardActions, QInput, QForm, QBtn']
+    components: [
+      'QCard',
+      'QCardSection',
+      'QCardActions, QInput, QForm, QBtn, QToolbar',
+      'QToolbarTitle',
+      'QBtnDropdown',
+      'QLayout',
+      'QPageContainer',
+      'QPage',
+      'QHeader',
+      'QFooter',
+      'QDrawer',
+      'QPageSticky',
+      'QPageScroller',
+      'QAvatar'
+    ]
   },
   plugins: ['Notify'],
   config: {
