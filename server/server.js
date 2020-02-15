2f408047e80b43fab5c32937b2f6d8d2fe2d050e
sasa02sasa <sasa02sasa@mail.ru>
2020-02-15 14:28:33 +0600

    Добавлена работа с сокетами на сервере (основа чата)

--- server/server.js
+++ server/server.js
@@ -2,16 +2,43 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 const app = express();
-const router = require('./router')
+const router = require('./router');
 const db = require('./db');
 const DB = new db();
+const path = require('path');
+let server = require('http').Server(app);
+let io = require('socket.io')(server);
+
+const publicPath = path.join(__dirname, '../public/');
+app.use(express.static(publicPath));
 
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.use(passport.initialize());
-app.use('/', router);
+app.use('/api', router);
 
-app.listen(3001, () => {
+io.on('connection', socket => {
+  console.log(Object.keys(io.sockets.connected).length);
+
+  socket.emit('connections', Object.keys(io.sockets.connected).length);
+
+  socket.on('chatMessage', data => {
+    console.log(data);
+    socket.broadcast.emit('chatMessage', data);
+  });
+
+  socket.on('joined', data => {
+    console.log('Joined', data);
+    socket.broadcast.emit('joined', data);
+  });
+
+  socket.on('leave', data => {
+    console.log('Leave', data);
+    io.sockets.emit('leave', data);
+  });
+});
+
+server.listen(3001, () => {
   DB.connect();
   console.log('We are live on ' + 3001);
 });
